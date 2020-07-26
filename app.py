5258b7bc439836b040996073edc77287204e72d8
sergio <sergio@localhost.localdomain>
2020-07-26 18:21:46 -0300

    trying to run the project

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+import time
+import playsound
+from gtts import gTTS
+import speech_recognition as sr
+
+def speak(text):
+    tts = gTTS(text=text, lang="en")
+    filename = "voice.mp3"
+    tts.save(filename)
+    playsound.playsound(filename)
+
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception: " + e)
+
+    return said
+
+# speak("hello sir")
+listen()
